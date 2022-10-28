a7b87c535309391e50c7600c3a9cc821ac936e01
AliciaV26 <84511171+AliciaV26@users.noreply.github.com>
2022-10-28 01:29:30 -0500

    Update guardar.js

--- JS/guardar.js
+++ JS/guardar.js
@@ -1,3 +1,4 @@
+//Newsletter
 function guardar(){
     email= document.getElementById("email").value;
 
@@ -21,11 +22,11 @@ function guardar(){
 }
 
 /*RESERVAR ENTRADAS*/
-function guardar1(){
-    nombre= document.getElementById("nombre").value;
-    email= document.getElementById("email").value;
-    cantidad_boleto= document.getElementById("cantidad_boleto").value;
-    fecha= document.getElementById("fecha").value;
+function reservar(){
+    const nombre= document.getElementById("nombre").value;
+    const email= auth.currentUser;
+    const cantidad_boleto= document.getElementById("cantidad_boleto").value;
+    const fecha= document.getElementById("fecha").value;
 
     valores_null = false;
 
@@ -55,7 +56,7 @@ function guardar1(){
     db.collection("reserva").add({
 
         nombre: document.getElementById("nombre").value,
-        email: document.getElementById("email").value,
+        email: auth.currentUser.email,
         cantidad_boleto: document.getElementById("cantidad_boleto").value,
         fecha: document.getElementById("fecha").value
     })
@@ -70,76 +71,7 @@ function guardar1(){
     }//if
 }
 
-/*REGISTRARSE */
-function guardar3(){
-    //console.log("Entra");
-    nombre= document.getElementById("nombre").value;
-    email= document.getElementById("email").value;
-    password= document.getElementById("password").value;
-
-    valores_null = false;
-    
-
-    if((nombre == "") || (email == "") || (password == "")){
-        alert("Complete los campos");
-        valores_null = true;
-    }
-    if(valores_null == false){
-    db.collection("usuario").add({
-
-        nombre: document.getElementById("nombre").value,
-        email: document.getElementById("email").value,
-        password: document.getElementById("password").value
-    })
-    .then((docRef) => {
-        alert("Se registró con éxito")
-        console.log("No error")
-        window.location.href = "https://aliciav26.github.io/"
-    })
-    .catch((error) => {
-        alert("Error. No se pudo registrar el usuario")
-        console.log("error")
-    });
-    }//if
-}
-
-/*INICIAR SESIÓN */
-function guardar2(){
-    
-        var usuarioRef = db.collection("usuario");
-        var login = true;
-        var passwordMatch;
-
-        passwordCheck = document.getElementById("password").value;
-        usuarioCheck = document.getElementById("nombre").value;
-
-        usuarioRef.where("nombre","==", usuarioCheck).get()
-        .then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                // doc.data() is never undefined for query doc snapshots
-                //console.log(doc.data().password); Mostrar datos
-                passwordMatch = doc.data().password;
-            });
-        })
-        .catch((error) => {
-            console.log("Error getting documents: ", error);
-            
-        });
 
 
-        setTimeout(function(){
-            if(passwordMatch != passwordCheck){
-                alert("Ingrese el usuario y/o contraseña correcta");
-                login = false;
-            }
-
-            if(login == true){
-                alert("Ingresó con éxito");
-                window.location.href = "https://aliciav26.github.io/"
-
-            }
-        }, 2000);
-
-}
 
 
